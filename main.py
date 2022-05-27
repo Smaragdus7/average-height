67047e2d9e3278a4ec285f9c0a1e47ec01fe3132
Smaragdus7 <smar.leeft@gmail.com>
2022-05-27 12:01:48 -0500

    finale

---
+++ main.py
@@ -0,0 +1,14 @@
+total_sum = 0
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this row 👇
+  total_sum += student_heights[n]
+print(round(total_sum/len(student_heights)))
+
+
+
